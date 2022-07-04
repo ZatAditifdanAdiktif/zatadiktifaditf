38b81e0cf5f2e75067cb0965a21e4adccbe593b1
ZatAditifdanAdiktif <93830666+ZatAditifdanAdiktif@users.noreply.github.com>
2022-07-04 23:27:02 +0800

    publish13

--- bahan/zat aditif/2.js
+++ bahan/zat aditif/2.js
@@ -3,22 +3,29 @@
 
 //navigasi gambar
 
-function gbr1(){
-  deklinasi.style.display="";
-  inklinasi.style.display="none";
+// function gbr1(){
+//   deklinasi.style.display="";
+//   inklinasi.style.display="none";
 
 
-  tombol1.style.background="black";
-  tombol2.style.background="#04709b";
+//   tombol1.style.background="black";
+//   tombol2.style.background="#04709b";
 
-}
-function gbr2(){
-deklinasi.style.display="none";
-inklinasi.style.display="";
+// }
+// function gbr2(){
+// deklinasi.style.display="none";
+// inklinasi.style.display="";
 
-  tombol1.style.background="#04709b";
-  tombol2.style.background="black";
-}
+//   tombol1.style.background="#04709b";
+//   tombol2.style.background="black";
+// }
+// function gbr3(){
+//   deklinasi.style.display="none";
+//   inklinasi.style.display="";
+  
+//     tombol1.style.background="#04709b";
+//     tombol2.style.background="black";
+//   }
 
 //Latihan 
 //Latihan
@@ -39,15 +46,28 @@ function cek2(){
 if(document.getElementById("s2benar").checked) {
      j2benar.style.display="";
      j2salah.style.display="none";
-     kesimpulan.style.display=""; //kesimpulan
-     sebelum.style.display="none"; //kesimpulan
+     pertanyaan3.style.display="";
+   
  }   
  if(document.getElementById("s2salah").checked){
      j2salah.style.display="";
      j2benar.style.display="none";
+     
 
  }
 }
+
+function cek3(){
+  if(document.getElementById("s3benar").checked) {
+       j3benar.style.display="";
+       j3salah.style.display="none";
+       kesimpulan.style.display="";
+   }   
+   if(document.getElementById("s3salah").checked){
+       j3salah.style.display="";
+       j3benar.style.display="none";
+   }
+ }
 
 function selesaibaca(){
 
